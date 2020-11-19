10ae0ceeb387aeab6c774939312d93bf89f07e88
Stefan <lazeastefan@gmail.com>
2020-11-19 21:12:20 +0200

    Init

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// router.use("/auth", authRouter);
-router.get('/test', (req, res) => {
-    res.send({ message: "ok" })
-})
-
-module.exports = router;
