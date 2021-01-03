fbcdbf9304b423171c27837453571faab043740d
Stefan <lazeastefan@gmail.com>
2021-01-03 21:26:32 +0200

    Add getting token in the request

--- frontend/src/service/CarService.js
+++ frontend/src/service/CarService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class CarService {
   getCars() {
     return axios.get("http://localhost:3001/api/part", {
-      headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlYyQUVlNDlsRjY3UXVOOVgzbkNFIiwiaWF0IjoxNjA5NjkxOTgzLCJleHAiOjE2MDk3MDI3ODN9.BfOSzYqvSyh7IO210NyDQZhq60pHBf3a5OTshi1r0Tg" }
+      headers: { "Authorization": localStorage.getItem("token") }
     }).then(res => res.data);
   }
 }
