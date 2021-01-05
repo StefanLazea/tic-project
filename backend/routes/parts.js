a8ffc23c68cf4e000ae4d1165855cb6565b10708
Stefan <lazeastefan@gmail.com>
2021-01-05 22:20:09 +0200

    CRUD on parts

--- backend/routes/parts.js
+++ backend/routes/parts.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { authorize } = require('../middlewares/authorize')
 const PartsController = require('../controllers/parts')
 
-router.post("/", authorize, (req, res) => {
-    return res.send({ message: "henlo" })
-});
+router.post("/", authorize, PartsController.savePart);
 router.get('/', PartsController.getAllParts);
+router.put('/:partId', authorize, PartsController.updateParts);
+router.delete('/:partId', authorize, PartsController.deletePart);
 
 module.exports = router;
